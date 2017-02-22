62e57cbaab3c1b50335606e14ce87ab44adaa909
bloomfieldfong <bloomfieldf24@gmail.com>
2017-02-22 14:59:03 -0600

    ...

--- src/iCalculadora.java
+++ src/iCalculadora.java
@@ -1,4 +1,45 @@
 
-public class iCalculadora {
+/**
+ * @author Rodrigo Juarez Jui 16073
+ * @author Michelle Bloomfield Fong 16803
+ *
+ */
 
+
+public interface iCalculadora {
+
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double suma(double a, double b);
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double resta(double a, double b);
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double multiplicacion(double a, double b);
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double division(double a, double b);
+	
+	/**
+	 * @param n
+	 * @return
+	 */
+	public double operar (String n);
+	
 }
--- src/iPila.java
+++ src/iPila.java
@@ -1,4 +1,25 @@
 
-public class iPila {
+/**
+ * @author Rodrigo Juarez Jui 16073
+ * @author Michelle Bloomfield Fong 16803
+ *
+ */
 
+public interface iPila<E>{
+	
+	/**
+	 * @param algo
+	 */
+	public void push(E algo);
+	
+	/**
+	 * @return
+	 */
+	public E pop();
+	
+	/**
+	 * @return
+	 */
+	public boolean isEmpty();
+	
 }
